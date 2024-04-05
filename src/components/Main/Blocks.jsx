090e9081852f34e95b8ccf0cdcf3730f2a69dc32
Dibrov Gleb <g.t.ru@mail.ru>
2024-04-05 14:56:53 +0300

    big update

--- src/components/Main/Blocks.jsx
+++ src/components/Main/Blocks.jsx
@@ -1,13 +1,13 @@
+
 export default function Blocks(props) {
     const {title, description, btntext, img} = props;
-  
+
     return(
         <div>
             <h3>{title}</h3>
             <p>{description}</p>
             <button>{btntext}</button>
-            <img src={img} alt=""/>
-            
+            <img src={img} alt={img}/>
         </div>
     )
   }
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,11 +1,15 @@
-import Blocks from './Blocks'
+import Blocks from './Blocks';
 import data from "./Content.json";
+import './Main.sass';
 
 export default function Main() {
+
     return (
         <main>
             <h1>Теперь услуги дилерского центра Вы можете можете получить онлайн!</h1>
-            {data.map(x=> <Blocks key={x.id} title={x.title} description={x.description} btntext={x.btntext} img={x.img}/>)}
+            <div className='container'>
+                {data.map(x=> <Blocks key={x.id} title={x.title} description={x.description} btntext={x.btntext} img={x.img}/>)}
+            </div>
         </main>
     )
 }
